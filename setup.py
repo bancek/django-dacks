4071426e7fb569872c73031874c7ebcc7de2fdfb
Luka Zakrajsek <luka@bancek.net>
2011-06-15 13:40:08 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-dacks',
+    version=__import__('dacks').__version__,
+    description='Django hacks for faster coding.',
+    author='Luka Zakrajsek',
+    author_email='luka@bancek.net',
+    url='https://github.com/bancek/django-dacks',
+    download_url='https://github.com/bancek/django-dacks',
+    license='Django',
+    packages=find_packages(exclude=['ez_setup']),
+    include_package_data=True,
+    zip_safe=True,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: Django License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
